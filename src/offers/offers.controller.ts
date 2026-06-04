7c12a3a059b2ec97fb90d9b1660475aa48f48c17
Melanie <git@melanie-busse.de>
2026-06-04 14:39:02 +0200

    Feature: add offers service and controller

--- src/offers/offers.controller.ts
+++ src/offers/offers.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  ParseIntPipe,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+} from '@nestjs/common';
+import { OffersService } from './offers.service';
+import { CreateOfferDto } from './dto/create-offer.dto';
+import { plainToInstance } from 'class-transformer';
+import { OfferResponseDto } from './dto/offer-response.dto';
 
 @Controller('offers')
-export class OffersController {}
+@UseInterceptors(ClassSerializerInterceptor) // Filtert die Daten anhand der @Expose() Regeln im DTO
+export class OffersController {
+  constructor(private readonly offersService: OffersService) {}
+
+  // POST /offers -> Ein neues Gebot für eine Auktion abgeben
+  @Post()
+  async create(
+    @Body() createOfferDto: CreateOfferDto,
+  ): Promise<OfferResponseDto> {
+    const offer = await this.offersService.create(createOfferDto);
+
+    return plainToInstance(OfferResponseDto, offer);
+  }
+
+  // GET /offers/auction/:auctionId -> Den gesamten Gebotsverlauf einer bestimmten Auktion abrufen
+  @Get('auction/:auctionId')
+  async findByAuction(
+    @Param('auctionId', ParseIntPipe) auctionId: number,
+  ): Promise<OfferResponseDto[]> {
+    const offers = await this.offersService.findByAuction(auctionId);
+    return plainToInstance(OfferResponseDto, offers);
+  }
+}
--- src/offers/offers.service.ts
+++ src/offers/offers.service.ts
@@ -1,4 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Offer } from './offers.entity';
+import { Repository } from 'typeorm';
+import { Auction } from '../auctions/auctions.entity';
+import { CreateOfferDto } from './dto/create-offer.dto';
 
 @Injectable()
-export class OffersService {}
+export class OffersService {
+  constructor(
+    @InjectRepository(Offer)
+    private readonly offerRepository: Repository<Offer>,
+    @InjectRepository(Auction)
+    private readonly auctionRepository: Repository<Auction>,
+  ) {}
+
+  async create(createOfferDto: CreateOfferDto): Promise<Offer> {
+    const { amount, offererId, auctionId } = createOfferDto;
+
+    const auction = await this.auctionRepository.findOne({
+      where: { id: auctionId },
+    });
+
+    if (!auction) {
+      throw new NotFoundException('Auction not found');
+    }
+
+    const now = new Date();
+
+    if (now > auction.endDate) {
+      throw new ConflictException(
+        'Gebot abgelehnt: Diese Auktion ist bereits beendet',
+      );
+    }
+
+    if (auction.currentprice) {
+      if (amount <= auction.currentprice) {
+        throw new ConflictException(
+          'Gebot abgelehnt. Preis ist niedriger als der aktuelle Preis',
+        );
+      }
+    } else {
+      if (amount < auction.startingprice) {
+        throw new ConflictException(
+          'Gebot abgelehnt. Preis ist niedriger als der Startbetrag',
+        );
+      }
+    }
+
+    const newOffer = this.offerRepository.create({
+      amount,
+      offererId,
+      auction,
+    });
+
+    const savedOffer = await this.offerRepository.save(newOffer);
+
+    auction.currentprice = amount;
+    await this.auctionRepository.save(auction);
+
+    return savedOffer;
+  }
+
+  async findByAuction(auctionId: number): Promise<Offer[]> {
+    return await this.offerRepository.find({
+      where: { auction: { id: auctionId } },
+      order: { createdAt: 'DESC' },
+    });
+  }
+}
