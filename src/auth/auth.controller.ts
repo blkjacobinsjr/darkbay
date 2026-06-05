2404e48ec8ac592bcef3711f0a9e4f9b9542ae32
blkjacobinsjr <jr.a.schmalz@gmail.com>
2026-06-05 12:59:09 +0200

    feat: implement local authentication strategy and login endpoint using Passport and bcrypt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Injectable } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { User } from '../user/users.entity';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService, 
+        private readonly usersService: UserService
+    ) {}
+    @Post('login')
+    async login(@Body()loginDto: LoginDto): Promise<User> {
+        return this.authService.login(request.user);
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { async } from 'rxjs';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor (
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService,
+    ){}
+    
+    async validateUser(username: string, password: string): Promise<any> {
+        const user = await this.userService.findByUsername(username);
+        if (user && bcrypt.compare(password, user.password)) {
+            return user;
+        }
+        return null;
+    }
+
+    async login(user: {id: number; username: string}) {
+        const payload = { username: user.username, sub: user.id };
+        
+}
